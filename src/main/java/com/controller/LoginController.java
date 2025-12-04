34c7ff0766ee923f50553d6d9c7c6eec3b5802ea
ayeshlakshan35 <ayeshalakshan00@gmail.com>
2025-12-04 21:10:50 +0530

    update controller

--- src/main/java/com/controller/LoginController.java
+++ src/main/java/com/controller/LoginController.java
@@ -1,15 +1,17 @@
 package com.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
-import javafx.scene.Node;
 
 // ENCAPSULATION: This class demonstrates encapsulation by:
 // 1. Using private fields for UI components (TextField, PasswordField, Label)
@@ -47,11 +49,23 @@ public class LoginController {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(resource));
             Parent root = loader.load();
             Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            fitToScreen(stage);
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void fitToScreen(Stage stage) {
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        stage.setHeight(bounds.getHeight());
+        stage.setMaximized(true);
+        stage.centerOnScreen();
     }
 }
 
